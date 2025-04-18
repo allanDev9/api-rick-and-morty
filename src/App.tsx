b36fc7217899f3e343e941ffe90a3ba9278d8240
allanDev9 <allanpenaguerra2003@gmail.com>
2025-04-18 13:55:00 -0600

    ajuste del boton de los modos de claridad de pantalla

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ function App() {
     <>
       <div className={`flex justify-center flex-row bg-black p-10 text-center ${theme ? 'ligth-theme' : 'dark-theme'}`}>
         <div className='flex'>
-          <button className={`text-black h-[50px] w-[55px] rounded-4xl p relative left-275 max-sm:left-78 max-sm:w-[40px] max-sm:h-[35px] transition duration-300 ease-in-out ${theme ? 'bg-black text-white hover:bg-gray-800 border border-black ' : 'bg-white text-black hover:bg-gray-200'}`} onClick={handletoggleMode}>
+          <button className={`text-black h-[50px] w-[55px] rounded-4xl p relative left-275 max-sm:left-75 max-sm:w-[40px] max-sm:h-[35px] transition duration-300 ease-in-out ${theme ? 'bg-black text-white hover:bg-gray-800 border border-black ' : 'bg-white text-black hover:bg-gray-200'}`} onClick={handletoggleMode}>
             {theme ? <NightlightRoundIcon /> : <WbSunnyIcon />}
           </button>
         </div>
