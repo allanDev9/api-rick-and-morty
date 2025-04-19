a7642624b99f5e71cf3dd0694ec9f54aee81fa72
allanDev9 <allanpenaguerra2003@gmail.com>
2025-04-18 19:37:58 -0600

    ajustes de resolucion de pantalla tanto para movil de 400px como para pc de 1900px

--- src/components/pages/Characters.tsx
+++ src/components/pages/Characters.tsx
@@ -35,20 +35,37 @@ const Characters = ({ theme }: CharacterProps) => {
 
   return (
     <div className="max-sm:grid max-sm:grid-cols-1 max-sm:place-items-center max-sm:mr-7">
-      <div className={`text-4xl font-extrabold ${theme ? 'text-blue-900' : 'text-yellow-300'}`}>Api Rick And Morty</div>
-      <input className={`border mt-20 mr-200 p-2 w-[280px] rounded-4xl max-sm:mr-0 ${theme ? 'border-black text-black' : 'border-gray-400 text-white'}`} type="text" placeholder="Buscar personaje..." value={searchcharacter} onChange={handleSearchChange} />
-      <article className="grid grid-cols-3 gap-10 mt-10 max-sm:grid-cols-1">
-        {filterCharacter.map((character: CharactersType) => (
-          <section className={`text-start scale-95 font-bold rounded-3xl border border-blue-700 hover:scale-100 hover:border-white transition duration-300 ${theme ? 'text-black border-gray-700 bg-gray-400' : 'text-black bg-gray-300'}`} key={character.id}>
-            <img className='rounded-3xl hover:sepia-50 transition duration-300 ease-in-out' src={character.image} alt={character.name}></img>
-            <div className="flex flex-col">
-              <h2>{character.name}</h2>
-              <span>{character.status}</span>
-              <span>{character.species}</span>
-            </div>
-          </section>
-        ))}
-      </article>
+      <div className={`text-4xl font-extrabold ${theme ? 'text-blue-900' : 'text-yellow-300'}`}>
+        Api Rick And Morty
+      </div>
+      <input
+        className={`border mt-20 p-2 w-[280px] rounded-4xl max-sm:mr-0 ${theme ? 'border-black text-black' : 'border-gray-400 text-white'}`}
+        type="text"
+        placeholder="Buscar personaje..."
+        value={searchcharacter}
+        onChange={handleSearchChange}
+      />
+      <section className="flex flex-col h-[80vh] gap-10 mt-10 max-sm:flex-col max-sm:h-[80vh] max-sm:w-[100%] border-2 border-black rounded-xl max-w-[1000px] overflow-y-auto p-4">
+        {filterCharacter.length > 0 ? (
+          filterCharacter.map((character: CharactersType) => (
+            <li key={character.id} className="max-sm:ml-5 list-none bg-blue-400 max-sm:w-[200px] w-[300px] h-[400px] flex flex-col items-center rounded-md">
+              <img
+                src={character.image}
+                alt={character.name}
+                className="object-cover w-[298px] h-[26
+                80px] rounded-md" // Cambiado a object-contain
+              />
+              <div className="text-center p-5">
+                <p className="font-bold">{character.name}</p>
+                <p>{character.status}</p>
+                <p>{character.species}</p>
+              </div>
+            </li>
+          ))
+        ) : (
+          <p className="text-2xl text-red-600 font-mono h-full col-span-3">No se encontraron caracteres</p>
+        )}
+      </section>
     </div>
   )
 }
